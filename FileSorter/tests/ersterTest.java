1fb52a8c1ced548dcecfddae3655bf5158acf14e
MaxxBahr <schnee.bahr@googlemail.com>
2023-05-26 14:25:04 +0200

    Tests added

---
+++ FileSorter/tests/ersterTest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import functionality.filesinput.*;
+public class ersterTest {
+
+	@Test
+	public void test() {
+		FileReader f1 = new FileReader("C:\\Users\\bahr-\\Desktop\\folder");
+		f1.sort();
+	}
+
+}
