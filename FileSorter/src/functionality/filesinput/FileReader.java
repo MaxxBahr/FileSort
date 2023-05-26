e39b6eccde13e1c4052c9144158ddc6a58a402a2
MaxxBahr <schnee.bahr@googlemail.com>
2023-05-26 12:22:19 +0200

    more general logic

--- FileSorter/src/functionality/filesinput/FileReader.java
+++ FileSorter/src/functionality/filesinput/FileReader.java
@@ -5,27 +5,25 @@ import java.io.File;
 public class FileReader {
 
 	File sourceFile;
-	
+
 	public FileReader(String s) {
 		setFilePath(s);
 	}
 
 	public void sort() {
 		File[] fileList = sourceFile.listFiles();
-		for(int i = 0; i < fileList.length; i++) {
-			if(fileList[i].getName().contains("Mathe")) {
-				File destFolder = new File(sourceFile, "Mathe");
-				if(!destFolder.exists()) {
-					destFolder.mkdirs();
-				}
-				File destFile = new File(destFolder, fileList[i].getName());
-				fileList[i].renameTo(destFile);
+		for (int i = 0; i < fileList.length; i++) {
+			File destFolder = new File(sourceFile, fileList[i].getName().substring(0, 5));
+			if (!destFolder.exists()) {
+				destFolder.mkdirs();
 			}
+			File destFile = new File(destFolder, fileList[i].getName());
+			fileList[i].renameTo(destFile);
 		}
 	}
-	
+
 	private void setFilePath(String s) {
-		sourceFile = new File(s);	
+		sourceFile = new File(s);
 	}
 
 }
